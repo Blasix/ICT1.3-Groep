bdd963a5ae9dfc89f19c843bf64e5c96e79a48fd
JelvL <jvanleest02@gmail.com>
2025-03-19 16:11:17 +0100

    Added appointments

--- Assets/Scripts/AppointmentsScene/AppointmentsSceneManager.cs
+++ Assets/Scripts/AppointmentsScene/AppointmentsSceneManager.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+using Assets.Scripts.Objects;
 
 public class AppointmentsSceneManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public TMP_InputField AppointmentInputField;
+    public TMP_InputField DateInputField;
+    public GameObject[] appointmentItems;
+
+    [System.Serializable]
+    private class AppointmentListWrapper
     {
-        
+        public List<Appointment> appointments;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickSaveButton()
     {
-        
+        List<Appointment> appointments = new List<Appointment>();
+
+        for (int i = 0; i < appointmentItems.Length; i++)
+        {
+            TMP_Text[] texts = appointmentItems[i].GetComponentsInChildren<TMP_Text>();
+            Appointment appointment = new Appointment();
+            appointment.appointmentName = texts[0].text;
+            appointment.date = texts[1].text;
+            appointments.Add(appointment);
+        }
+
+        AppointmentListWrapper wrapper = new AppointmentListWrapper();
+        wrapper.appointments = appointments;
+        string json = JsonUtility.ToJson(wrapper);
+        PlayerPrefs.SetString("Appointments", json);
+        PlayerPrefs.Save();
     }
 }
