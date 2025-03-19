cb3d873a616e0c99cb1559ccb97347de45b31024
smickel20 <75023071+smickel20@users.noreply.github.com>
2025-03-19 11:59:54 +0100

    Welcome Scene

---
+++ Assets/Scripts/WelcomeScene/MovementWelcomeScene.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MovementWelcomeScene : MonoBehaviour
+{
+    public Button Loginbutton;
+    public Button Registerbutton;
+    public Image LoginbuttonImage;
+    public Image RegisterbuttonImage;
+    void Start()
+    {
+        LoginbuttonImage.color = new Color(0f, 0f, 0f, 0.5f);
+        RegisterbuttonImage.color = new Color(0f, 0f, 0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveToLoginScene()
+    {
+            SceneManager.LoadScene("LoginScene");
+    }
+
+    public void MoveToRegisterScene()
+    {
+            SceneManager.LoadScene("RegisterScene");
+    }
+}
