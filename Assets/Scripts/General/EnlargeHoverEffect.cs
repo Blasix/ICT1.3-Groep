23db7f283e6ab9e3727cd394ba0ba59055f5a11c
aiyalcin <ai.yalcin@student.avans.nl>
2025-04-04 23:01:16 +0200

    added sound effect on hover

--- Assets/Scripts/General/EnlargeHoverEffect.cs
+++ Assets/Scripts/General/EnlargeHoverEffect.cs
@@ -3,6 +3,7 @@ using UnityEngine.EventSystems;
 using DG.Tweening;
 
 [RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(AudioSource))]
 public class EnlargeHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("Scale Settings")]
@@ -10,17 +11,42 @@ public class EnlargeHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerE
     [SerializeField] private float scaleDuration = 0.2f;
     [SerializeField] private Ease scaleEase = Ease.OutBack;
 
+    [Header("Audio Settings")]
+    [SerializeField] private string audioClipName = "SpringSound"; // Name of the MP3 file in the Resources folder
+
     private Vector3 _originalScale;
     private Tween _currentTween;
+    private AudioSource _audioSource;
+    private AudioClip _audioClip;
 
     private void Awake()
     {
         // Store original scale
         _originalScale = transform.localScale;
+
+        // Get the AudioSource component
+        _audioSource = GetComponent<AudioSource>();
+
+        // Load the audio clip from the Resources folder
+        _audioClip = Resources.Load<AudioClip>(audioClipName);
+        if (_audioClip != null)
+        {
+            _audioSource.clip = _audioClip;
+        }
+        else
+        {
+            Debug.LogError($"Audio clip '{audioClipName}' not found in Resources folder.");
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        // Play the audio clip
+        if (_audioClip != null)
+        {
+            _audioSource.Play();
+        }
+
         // Kill any active tween first
         _currentTween?.Kill();
 
